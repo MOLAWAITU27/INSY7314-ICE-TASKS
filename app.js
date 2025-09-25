8d4be7a54d96318cd6dbd2f84d727b9c081eb412
MOLAWAITU27 <itumeleng.molawa@icloud.com>
2025-09-25 23:10:29 +0200

    Initial commit - PulseVote backend setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('PulseVote API running!');
+});
+
+app.get('/test', (req, res) => {
+  res.json({ message: 'Testing that PulseVote API running!' });
+});
+
+module.exports = app;
+ 
